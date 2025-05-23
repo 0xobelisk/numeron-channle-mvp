c796e73ae6f4b042ad155b40ac8020a153148e93
vladilen11 <vladilenaksana@163.com>
2025-05-23 22:13:14 +0800

    update numeron testnet deployment

--- packages/contracts/deployment.ts
+++ packages/contracts/deployment.ts
@@ -1,6 +1,6 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
-export const NETWORK: NetworkType = 'localnet';
-export const PACKAGE_ID = '0x8e87e0583de6142a608daa78d12515e8fc1b3197496148e5e817ddfba13d4dc5';
-export const SCHEMA_ID = '0xae5a702cafea9a8f12bfaea8f1f3e1bf6fc5912b3befc99b09ed64dc78fb707d';
-export const DUBHE_SCHEMA_ID = '0xde6226a4546781c2b15113c12a5552d659f14fc00fcf4f6b714c0e38fcbf4350';
+export const NETWORK: NetworkType = 'testnet';
+export const PACKAGE_ID = '0x96659c76955a3ba69d8e31f3d9c9f3b08825ebdad036327be3ee9c554a8169b7';
+export const SCHEMA_ID = '0xa47c0045427a870e5c05f1c9d2eff04547c5aba06b3b83abc4b3faeb0611f42c';
+export const DUBHE_SCHEMA_ID = '0x8ece4cb6de126eb5c7a375f90c221bdc16c81ad8f6f894af08e0b6c25fb50a45';
