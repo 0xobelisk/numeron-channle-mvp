d54f4ed29dd6afebc520d06497cd31807af12202
vladilen11 <vladilenaksana@163.com>
2025-11-16 14:20:07 +0800

    update version

--- packages/contracts/deployment.ts
+++ packages/contracts/deployment.ts
@@ -1,5 +1,5 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 export const NETWORK: NetworkType = 'localnet';
-export const PACKAGE_ID = '0x1ed66943c2e5ec5af056c4a9069c9f2d2e1080867d670940b535816f229de0eb';
-export const DUBHE_SCHEMA_ID = '0xa4b20425103e99233435dbad4c4fc5e422c02f605ebd85e9d10b172b6c27c15c';
+export const PACKAGE_ID = '0x964926fe6d8b7618f2fa71df4f3b382438218ed5f1588dd6dfb139c475a75940';
+export const DUBHE_SCHEMA_ID = '0x3b0afb135d457f9cea4f1333e12555e89777d6b0eb07d7e501b684e79e646214';
